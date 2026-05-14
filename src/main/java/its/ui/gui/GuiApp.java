0e28ca2f969ffdc39c021c9274c8814b1d0d6b53
thfdl <soliepark9204@cau.ac.kr>
2026-05-14 18:09:28 +0900

    feat: initialize swing GUI
    Initialized swing GUI with empty main frame

--- src/main/java/its/ui/gui/GuiApp.java
+++ src/main/java/its/ui/gui/GuiApp.java
@@ -1,4 +1,18 @@
 package its.ui.gui;
 
+import javax.swing.*;
+
 public class GuiApp {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
 }
---
+++ src/main/java/its/ui/gui/MainFrame.java
@@ -0,0 +1,19 @@
+package its.ui.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JPanel contentPanel;
+
+    public MainFrame() {
+        setTitle("ITS");
+        setSize(1200, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        initComponents();
+    }
+
+    private void initComponents() {}
+}
