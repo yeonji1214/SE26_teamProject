79485438fd9ac01a1c713b5e078fa29a73e8f63f
Ryu <chanchen44@gmail.com>
2026-05-18 08:43:10 +0900

    Add repository interfaces

--- src/main/java/its/repository/user/UserRepository.java
+++ src/main/java/its/repository/user/UserRepository.java
@@ -7,7 +7,10 @@ import java.util.Optional;
 
 public interface UserRepository {
     User save(User user);
+
     Optional<User> findById(Long id);
+
     Optional<User> findByUsername(String username);
+
     List<User> findAll();
 }
